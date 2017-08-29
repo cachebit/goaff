5dcd34e672242563117ec651c61c7540b36d2e1f
cachebit <417209993@qq.com>
2017-08-29 06:57:12 +0000

    301 added

--- routes/web.php
+++ routes/web.php
@@ -163,4 +163,241 @@ Route::get('/articles/{id}/{slug?}', "TopicsController@show")->name('articles.sh
 
 # ------------------ Wild Card ------------------------
 
+
 Route::get('{name}', 'PagesController@wildcard')->name('wildcard');
+
+
+
+Route::get('/tutorial',function(){
+    return redirect('/categories/16', 301);
+});
+Route::get('/category/guide',function(){
+    return redirect('/categories/17', 301);
+});
+Route::get('/state-abbr',function(){
+    return redirect('/topics/1/guo-jia-he-di-qu-suo-xie-dai-ma-he-shi-cha', 301);
+});
+Route::get('/category/nuts',function(){
+    return redirect('/topics', 301);
+});
+Route::get('/category/basic',function(){
+    return redirect('/categories/5', 301);
+});
+Route::get('/category/wiki',function(){
+    return redirect('/categories/4', 301);
+});
+Route::get('/category/stuff',function(){
+    return redirect('/categories/7', 301);
+});
+Route::get('/category/site',function(){
+    return redirect('/categories/6', 301);
+});
+Route::get('/how-to-started-in-media-buy-1',function(){
+    return redirect('/topics/2/kai-shi-media-buy-yi-gei-media-buy-xin-shou-de-4-ge-ti-shi', 301);
+});
+Route::get('/affiliate-marketing-key-points-1',function(){
+    return redirect('/topics/3/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-1-ru-he-gao-xiao-xuan-ze-offer', 301);
+});
+Route::get('/popads-create-new-campaign-tutorial-2017',function(){
+    return redirect('/topics/4/popads-ru-he-xin-jian-campaign-tu-wen-jiao-cheng-2017-nian-geng-xin', 301);
+});
+Route::get('/what-is-cta',function(){
+    return redirect('/topics/5/shen-me-shi-cta', 301);
+});
+Route::get('/how-tp-started-media-buy-2-adult-ad-network-analysis-part-one',function(){
+    return redirect('/topics/6/kai-shi-media-buy-er-da-ren-ad-network-liu-liang-ping-tai-fen-xi', 301);
+});
+Route::get('/affiliate-marketing-key-points-2',function(){
+    return redirect('/topics/7/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-2-ru-he-xuan-ze-liu-liang-ping-tai', 301);
+});
+Route::get('/what-is-affiliate-marketing',function(){
+    return redirect('/topics/8/shen-me-shi-affiliate-marketing', 301);
+Route::get('/what-is-ad-network',function(){
+    return redirect('/topics/9/shen-me-shi-ad-network', 301);
+});
+Route::get('/amazon-associates-policy-1',function(){
+    return redirect('/topics/10/amazon-associates-zheng-ce-1-jian-jie', 301);
+});
+Route::get('/3x-ad-optimize-system',function(){
+    return redirect('/topics/11/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-3-3-bu-zou-wen-bu-ce-shi-campaign-huo-li', 301);
+});
+Route::get('/how-tp-started-media-buy-2-mistakes-1',function(){
+    return redirect('/topics/12/kai-shi-media-buy-san-xin-shou-chang-jian-cuo-wu-1', 301);
+});
+Route::get('/affiliate-marketing-copy-landing-pages',function(){
+    return redirect('/topics/13/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-4-ru-he-copy-landing-pages', 301);
+});
+Route::get('/landing-pages',function(){
+    return redirect('/topics/14/shen-me-shi-landing-pages', 301);
+});
+Route::get('/a-complete-guide-to-affiliate-marketing-finch-1-introduction',function(){
+    return redirect('/topics/15/stm-lun-tan-da-shen-finch-li-zuo-affiliate-marketing-wan-quan-zhi-nan-lian-zai-geng-xin', 301);
+});
+Route::get('/a-complete-guide-to-affiliate-marketing-finch-2-what-do-affiliates-do',function(){
+    return redirect('/topics/16/affiliate-shi-zuo-shi-mo-de-affiliate-marketing-wan-quan-zhi-nan-1', 301);
+});
+Route::get('/what-is-pops-traffic-source',function(){
+    return redirect('/topics/17/xin-shou-fu-yin-ni-zhi-bu-zhi-dao-de-pop-liu-liang-da-qi-di', 301);
+});
+Route::get('/what-is-mobile-popunder-traffic',function(){
+    return redirect('/topics/18/pop-liu-liang-da-qi-di-1-yi-dong-duan-pops-guang-gao-shi-shen-me-yang-de-ta-men-ru-he-gong-zuo', 301);
+});
+Route::get('/pop-traffic-source-is-good-for-affiliate-newbie',function(){
+    return redirect('/topics/19/pop-liu-liang-da-qi-di-2-yi-dong-duan-di-pop-guang-gao-shi-he-xin-shou-ma', 301);
+});
+Route::get('/the-commission-models',function(){
+    return redirect('/topics/20/wo-men-ru-he-huo-de-bao-chou-yong-jin-mo-shi-affiliate-marketing-wan-quan-zhi-nan-2', 301);
+});
+Route::get('/stm-open-course-6wamc-4-step-affiliate-marketing-tutorial',function(){
+    return redirect('/topics/21/stm-lun-tan-gong-kai-ke-1-xin-ren-ru-xing-de-4-bu-ji-hua', 301);
+});
+Route::get('/affiliates-personalities',function(){
+    return redirect('/topics/22/shen-me-yang-de-ren-shi-he-dang-affiliate-affiliate-marketing-wan-quan-zhi-nan-3', 301);
+});
+Route::get('/affiliate-prepare',function(){
+    return redirect('/topics/23/affiliate-marketing-zhun-bei-gong-zuo-affiliate-marketing-wan-quan-zhi-nan-4', 301);
+});
+Route::get('/prepare-for-mobile-affiliate-marketing',function(){
+    return redirect('/topics/24/pop-liu-liang-da-qi-di-3-mobile-affiliate-kai-shi-qian-de-zhun-bei', 301);
+});
+Route::get('/which-offers-to-run-on-mobile-popup-and-popunder',function(){
+    return redirect('/topics/25/pop-liu-liang-da-qi-di-4-nei-xie-offers-verticals-zai-mobile-pops-shang-pao-de-bi-jiao-hao', 301);
+});
+Route::get('/pros-and-consof-mobile-popup-and-popunder',function(){
+    return redirect('/topics/26/pop-liu-liang-da-qi-di-5-yi-dong-duan-popup-he-popunder-de-you-he-lie', 301);
+});
+Route::get('/cash-flow-and-budget-in-affiliate-marketing',function(){
+    return redirect('/topics/27/xian-jin-liu-affiliate-marketing-zhong-ru-he-fen-pei-yu-suan-affiliate-marketing-wan-quan-zhi-nan-5', 301);
+});
+Route::get('/the-relations-between-affiliates',function(){
+    return redirect('/topics/28/ni-he-qi-ta-affiliates-de-guan-xi-affiliate-marketing-wan-quan-zhi-nan-6', 301);
+});
+Route::get('/the-relations-between-affiliate-and-affiliate-networks',function(){
+    return redirect('/topics/29/ni-he-affiliate-network-de-guan-xi-affiliate-marketing-wan-quan-zhi-nan-7', 301);
+});
+Route::get('/how-to-register-popads',function(){
+    return redirect('/topics/30/popads-zhu-ce-liu-cheng-jiao-ni-popads-zen-me-zhu-ce', 301);
+});
+Route::get('/popads-inroduction-and-faq',function(){
+    return redirect('/topics/31/popads-jian-jie-he-chang-jian-wen-ti', 301);
+});
+Route::get('/relations-between-affiliate-and-merchants',function(){
+    return redirect('/topics/32/ni-he-guang-gao-zhu-de-guan-xi-affiliate-marketing-wan-quan-zhi-nan-8', 301);
+});
+Route::get('/afflow-introduction-and-faq',function(){
+    return redirect('/topics/33/afflow-monetizer-jian-jie-he-chang-jian-wen-ti', 301);
+});
+Route::get('/how-to-register-afflow',function(){
+    return redirect('/topics/34/afflow-zhu-ce-liu-cheng-jiao-ni-afflow-zen-me-zhu-ce', 301);
+});
+Route::get('/relations-between-affiliate-and-traffic-source',function(){
+    return redirect('/topics/35/ni-he-liu-liang-ping-tai-de-guan-xi-affiliate-marketing-wan-quan-zhi-nan-9', 301);
+});
+Route::get('/relations-between-affiliates-and-end-users',function(){
+    return redirect('/topics/36/ni-he-zhong-duan-yong-hu-de-guan-xi-affiliate-marketing-wan-quan-zhi-nan-10', 301);
+});
+Route::get('/what-offers-verticals-should-i-promote',function(){
+    return redirect('/topics/37/xin-shou-gai-pao-shen-me-offer-shen-me-offer-hao-pao-affiliate-marketing-wan-quan-zhi-nan-11', 301);
+});
+Route::get('/verticals-affiliate-marketing-health',function(){
+    return redirect('/topics/38/affiliate-marketing-zhong-de-jian-kang-chan-pin-affiliate-marketing-wan-quan-zhi-nan-12', 301);
+});
+Route::get('/self-actualization-products-in-affiliate-marketing',function(){
+    return redirect('/topics/39/zi-wo-ti-sheng-lei-de-chan-pin-affiliate-marketing-wan-quan-zhi-nan-13', 301);
+});
+Route::get('/wealth-vertical-in-affiliate-marketing',function(){
+    return redirect('/topics/40/jin-rong-li-cai-lei-chan-pin-affiliate-marketing-wan-quan-zhi-nan-14', 301);
+});
+Route::get('/dating-vertical-in-affiliate-marketing',function(){
+    return redirect('/topics/41/yue-hui-she-jiao-lei-chan-pin-affiliate-marketing-wan-quan-zhi-nan-15', 301);
+});
+Route::get('/finch-sells-premium-posts-2016',function(){
+    return redirect('/topics/42/2016-nian-gan-huo-he-ji-finch-sells-premium-posts', 301);
+});
+Route::get('/affiliate-marketing-adults-verticals',function(){
+    return redirect('/topics/43/da-ren-lei-chan-pin-affiliate-marketing-wan-quan-zhi-nan-16', 301);
+});
+Route::get('/what-is-cloaking-what-is-cloak',function(){
+    return redirect('/topics/44/shen-me-shi-cloaking-shen-me-shi-cloak-2016-premium-posts-finch-1', 301);
+});
+Route::get('/insurance-vertical-in-affiliate-marketing',function(){
+    return redirect('/topics/45/bao-xian-lei-chan-pin-affiliate-marketing-wan-quan-zhi-nan-17', 301);
+});
+Route::get('/finance-vertical-in-affiliate-marketing',function(){
+    return redirect('/topics/46/jin-rong-ling-yu-chan-pin-affiliate-marketing-wan-quan-zhi-nan-18', 301);
+});
+Route::get('/gambling-vertical-in-affiliate-marketing',function(){
+    return redirect('/topics/47/bo-cai-lei-chan-pin-affiliate-marketing-wan-quan-zhi-nan-19', 301);
+});
+Route::get('/sweepstakes-in-affiliate-marketing',function(){
+    return redirect('/topics/48/chou-jiang-lei-affiliate-marketing-wan-quan-zhi-nan-20', 301);
+});
+Route::get('/penny-auction-affiliate-marketing',function(){
+    return redirect('/topics/49/yi-fen-qian-jing-pai-affiliate-marketing-wan-quan-zhi-nan-21', 301);
+});
+Route::get('/gamin-vertical-in-affiliate-marketing',function(){
+    return redirect('/topics/50/you-xi-lei-affiliate-marketing-wan-quan-zhi-nan-22', 301);
+});
+Route::get('/affiliate-marketing-by-lead-generation',function(){
+    return redirect('/topics/51/yin-dao-tui-jie-ying-li-affiliate-marketing-wan-quan-zhi-nan-23', 301);
+});
+Route::get('/sticking-to-a-single-vertical-as-affiliate',function(){
+    return redirect('/topics/52/zhuan-zhu-yu-yi-ge-ling-yu-affiliate-marketing-wan-quan-zhi-nan-24', 301);
+});
+Route::get('/mobile-affiliate-marketing',function(){
+    return redirect('/topics/53/ben-xiang-yi-dong-hu-lian-wang-zui-da-de-zhuan-qian-ji-hui-fu-xian-affiliate-marketing-wan-quan-zhi-nan-25', 301);
+});
+Route::get('/the-other-half-of-making-money',function(){
+    return redirect('/topics/54/zhuan-qian-de-ling-yi-ban-mi-jue', 301);
+});
+Route::get('/affiliate-marketing-%E7%9A%84%E5%87%A0%E4%B8%AA%E5%85%B3%E9%94%AE%E9%97%AE%E9%A2%98%EF%BC%885%EF%BC%89%EF%BC%9A%E8%8E%B7%E5%BE%97%E5%85%8D%E8%B4%B9angles%E5%9B%BE%E5%83%8Flanding-page',function(){
+    return redirect('/topics/55/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-5-huo-de-mian-fei-angles-tu-xiang-landing-page', 301);
+});
+Route::get('/arbitrage-affiliate-marketing',function(){
+    return redirect('/topics/56/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-6-tao-li-arbitrage', 301);
+});
+Route::get('/reset-your-goal-in-affiliate-marketing',function(){
+    return redirect('/topics/57/affiliate-marketing-de-ji-ge-guan-jian-wen-ti-7-kui-sun-he-bu-qie-shi-ji-de-qi-wang', 301);
+});
+Route::get('/meeage-match-of-google-adwords-ppc',function(){
+    return redirect('/topics/58/google-adwords-ppc-guang-gao-de-message-match-xin-xi-pi-pei', 301);
+});
+Route::get('/win-in-popup',function(){
+    return redirect('/topics/59/zheng-fu-pop-guang-gao-2016-premium-posts-finch', 301);
+});
+Route::get('/mobile-vs-web-in-affiliate-marketing',function(){
+    return redirect('/topics/60/yi-dong-hu-lian-wang-he-chuan-tong-hu-lian-wang-de-cha-yi-affiliate-marketing-wan-quan-zhi-nan-26', 301);
+});
+Route::get('/app-installs-in-affiliate-markeying',function(){
+    return redirect('/topics/61/app-installs-affiliate-marketing-wan-quan-zhi-nan-27', 301);
+});
+Route::get('/pin-submits-in-affiliate-marketing',function(){
+    return redirect('/topics/62/pin-submits-affiliate-marketing-wan-quan-zhi-nan-28', 301);
+});
+Route::get('/pay-per-call-in-affiliate-marketing',function(){
+    return redirect('/topics/63/pay-per-call-affiliate-marketing-wan-quan-zhi-nan-29', 301);
+});
+Route::get('/traditional-leadgen-in-affiliate-marketing',function(){
+    return redirect('/topics/64/chuan-tong-leadgen-shi-shen-me-affiliate-marketing-wan-quan-zhi-nan-30', 301);
+});
+Route::get('/propellerads-mobile-pop-case-study',function(){
+    return redirect('/topics/65/propellerads-yi-dong-pop-guang-gao-de-an-li', 301);
+});
+Route::get('/propellerads-register',function(){
+    return redirect('/topics/66/propellerads-zhu-ce-liu-cheng-jiao-ni-propellerads-zen-me-zhu-ce', 301);
+});
+Route::get('/traditional-sales-in-affiliate-marketing',function(){
+    return redirect('/topics/67/chuan-tong-xiao-shou-pay-per-sale-shi-shen-me-affiliate-marketing-wan-quan-zhi-nan-31', 301);
+});
+Route::get('/where-to-start-mobile-affiliate',function(){
+    return redirect('/topics/68/gei-mobile-affiliate-xin-ren-de-an-li-affiliate-marketing-wan-quan-zhi-nan-32', 301);
+});
+Route::get('/learn-the-chains-of-communication-with-affiliate-manager-and-merchants',function(){
+    return redirect('/topics/69/xue-hui-he-am-he-guang-gao-zhu-gou-tong-affiliate-marketing-wan-quan-zhi-nan-33', 301);
+});
+Route::get('/popads-setting-experience',function(){
+    return redirect('/topics/70/popads-zen-me-pao-geng-zhuan-qian', 301);
+});
+Route::get('/type-of-traffic',function(){
+    return redirect('/topics/71/liu-liang-de-zhong-yao-lei-xing-ru-he-fa-hui-ta-men-xiao-yong-affiliate-marketing-wan-quan-zhi-nan-34', 301);
+});
