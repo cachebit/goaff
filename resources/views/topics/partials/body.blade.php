74063597f7cbc296a46d72e84629845bd18821a0
cachebit <417209993@qq.com>
2017-08-29 08:59:24 +0000

    301 added

--- resources/views/topics/partials/body.blade.php
+++ resources/views/topics/partials/body.blade.php
@@ -5,8 +5,8 @@
 @if(isset($unaccessable) && $unaccessable)
   <div class="well">
     <p>
-        本站为 Affilaite Marketing 和 Media Buy 付费分享论坛。实时更新国外最新教程、资讯。欢迎加入，详见：
-        <a href="/topics/73/gai-ban-shuo-ming">改版说明</a>
+        <a href="/topics/73/gai-ban-shuo-ming">点击加入</a> Affilaite Marketing 和 Media Buy 付费分享论坛。实时更新国外最新教程、资讯。
+
     </p>
   </div>
 @endif
