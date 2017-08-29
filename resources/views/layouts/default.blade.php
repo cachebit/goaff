a0ed5a77e93aff636b91865e5c2312f644d48466
cachebit <417209993@qq.com>
2017-08-29 16:51:36 +0000

    add baidu stats

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -112,6 +112,15 @@
         @endif
 
 
+				<script>
+				var _hmt = _hmt || [];
+				(function() {
+				  var hm = document.createElement("script");
+				  hm.src = "https://hm.baidu.com/hm.js?0d3cd01e8dac9d45909850de5f3412c5";
+				  var s = document.getElementsByTagName("script")[0];
+				  s.parentNode.insertBefore(hm, s);
+				})();
+				</script>
 
 	</body>
 </html>
