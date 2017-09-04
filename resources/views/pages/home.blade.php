60ba7f06d8d145cee88b941ccb2d40c63b486c0a
cachebit <417209993@qq.com>
2017-09-04 02:30:21 +0000

    add email subscribe

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,6 +6,8 @@
   {!! lang('site_intro') !!}
 </div>
 
+<div id="sendcloud-embed-subscribe" style="margin-bottom:20px"></div>
+
 @include('layouts.partials.topbanner')
 
 <div class="panel panel-default list-panel home-topic-list">
@@ -19,16 +21,30 @@
 
   </div>
 
+  <div class="panel-footer text-right">
+  <br/>
+
   <div class="panel-body ">
 	@include('pages.partials.topics')
   </div>
-
-  <div class="panel-footer text-right">
 
   	<a href="{{ route('topics.free_topics') }}" class="more-excellent-topic-link">
   		所有公开文章 <i class="fa fa-arrow-right" aria-hidden="true"></i>
   	</a>
   </div>
 </div>
+
+<script src="http://sendcloud.sohu.com/js/subscribe.js"></script>
+<script>
+    var option = {
+    type: 'embed',
+    expires: '86400000',
+    trigger: 'load',
+    invitecode: '074a2db7-e3d5-4e80-9c6e-82a99d343cb7',
+    title: '不想错过任何动态？邮件订阅我们吧！',
+    successMsg: '请登录邮箱，点击确认订阅链接，即可订阅成功啦'
+    }
+    sendcloud.subscribe(option)
+</script>
 
 @stop
